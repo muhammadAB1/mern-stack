60bd8523175910d0f972e90241be72a54efaf4b7
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-04-22 23:53:56 +0500

    Update server.js

--- TodoList/api/server.js
+++ TodoList/api/server.js
@@ -1,48 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 
-mongoose.connect('mongodb://localhost:27017/mern-todo')
-.then(() => console.log('connect to db'))
-.catch(console.log(console.error))
+mongoose.connect('mongodb://localhost:27017/todolist')
+.then(() => console.log('connected to DB'))
+.catch(console.error())
 
 const Todo = require('./models/Todo')
 
 app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
+    const data = await Todo.find()
 
-    res.json(todos);
+    res.json(data)
 });
 
+
 app.post('/todo/new', (req, res) => {
+    if(todo.body === '')return;
     const todo = new Todo({
         text: req.body.text
-    });
+    })
 
-    todo.save();
-
-    res.json(todo);
+    todo.save()
+    res.json(todo)
 });
 
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id)
-
-    res.json(result);
+    try{
+    const data = await Todo.findByIdAndDelete(req.params.id)
+    res.json(data);
+    console.log(`deleted ${req.params.id}`)
+    } catch(e){
+        console.error(e)
+    }
 });
 
-app.get('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
-
-    todo.complete = !todo.complete
-
-    todo.save()
-
-    res.json(todo)
+app.put('/todo/complete/:id', async (req, res) => {
+    try{
+        const data = await Todo.findById(req.params.id)
+        data.complete = !data.complete
+        data.save()
+        res.json(data)
+    }catch(e){
+        console.log(e)
+    }
 });
+
+app.get('/todos/:id', async (req, res) => {
+    const data = await Todo.findById(req.params.id)
+    res.json(data)
+})
 
 app.listen(3001, () => console.log('server started on port 3001'));
