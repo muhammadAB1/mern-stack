8e135e0a140a39d32c1b73b2ba0d8a986d0305c6
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-04-29 19:36:15 +0500

    Add files via upload

---
+++ todolist/api/server.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb://localhost:27017/todolist')
+.then(() => console.log('connected to DB'))
+.catch(console.error())
+
+const Todo = require('./models/Todo')
+
+app.get('/todos', async (req, res) => {
+    const data = await Todo.find()
+
+    res.json(data)
+});
+
+
+app.post('/todo/new', (req, res) => {
+    if(req.body.text === '')return;
+    const todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save()
+    res.json(todo)
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    try{
+    const data = await Todo.findByIdAndDelete(req.params.id)
+    res.json(data);
+    console.log(`deleted ${req.params.id}`)
+    } catch(e){
+        console.error(e)
+    }
+});
+
+app.get('/todo/complete/:id', async (req, ) => {
+    try {
+        const data = await Todo.findById(req.params.id)
+        data.complete = !data.complete
+        data.save()
+        res.json(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+});
+
+app.get('/todos/:id', async (req, res) => {
+    const data = await Todo.findById(req.params.id)
+    res.json(data)
+})
+
+app.listen(3001, () => console.log('server started on port 3001'));
---
+++ todolist/client/src/App.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const api = 'http://localhost:3001'
+
+  const [todos, setTodos] = useState([])
+  const [popupActive, setPopupActive] = useState('')
+  const [newTodo, setNewTodo] = useState('')
+
+  useEffect(() => {
+    getTodos();
+  }, [])
+
+  const getTodos = () => {
+    fetch(api + '/todos')
+    .then(res => res.json())
+    .then(data => setTodos(data))
+    .catch(err => console.error('error', err))
+  }
+
+  const completeTodo = async id => {
+    const data = await fetch(api + '/todo/complete/' + id)
+    .then(res => res.json())
+    setTodos(todos => todos.map(todo => {
+      if(todo._id === data._id){
+        todo.complete = data.complete;
+      }
+      return todo;
+    }));
+  }
+  
+  const deleteTodo = async id => {
+    try{
+    const data = await fetch(api + '/todo/delete/' + id, {method: 'DELETE'})
+    .then(res => res.json())
+    setTodos(todos => todos.filter(todo => todo._id !== id))
+    } catch (e){
+      console.error(e)
+    }
+  }
+
+  const addTodo = async () =>{
+    const data = await fetch(api + '/todo/new', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json());
+    setTodos([...todos, data])
+    setPopupActive(false)
+    setNewTodo('')
+  }
+
+  return (
+    <div className="App">
+      <h1>Welcome Muhammad</h1>
+      <h3>Muhammad's Todo List</h3>
+
+
+      <div className = 'todos'>
+        {todos.map(todo => (
+          <div className={'todo ' + (todo.complete ? 'completed' : '')} 
+          key={todo._id}
+          onClick={() => {completeTodo(todo._id)}}>
+            <div className = 'checkbox'></div>
+            <div className = 'text'>{todo.text}</div>
+            <div className = 'delete' onClick={() => deleteTodo(todo._id)}>X</div>
+          </div>
+        ))}
+      </div>
+      <div className='addpopup' onClick={() => setPopupActive(true)}>+</div>
+      {popupActive ? (
+        <div className='popup'>
+          <div className='closepopup' onClick={() => setPopupActive(false)}>x</div>
+          <div className='content'>
+            <h3>Add Task</h3>
+            <input
+            type='text'
+            className='add-todo-input' onChange={e => setNewTodo(e.target.value)}
+            autoFocus
+            />
+            <div className='button' onClick={addTodo} tabIndex={'0'}>Create Task</div>
+          </div>
+        </div>
+      ): ''}
+    </div>
+  );
+}
+
+export default App;
