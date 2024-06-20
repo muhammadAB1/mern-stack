87f817dce036b745eba7442aa2e7d392936d9bab
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-06-20 20:43:13 +0500

    Update index.js

--- final project/quiz/models/index.js
+++ final project/quiz/models/index.js
@@ -3,7 +3,7 @@ import { Login } from './Login.js'
 import { Score } from './Score.js'
 import { Quiz } from './Quiz.js'
 
-mongoose.connect('mongodb://localhost:27017/quiz')
+mongoose.connect('mongodb+srv://muhammad:Digimon03.@cluster0.dek5f6q.mongodb.net/quiz?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('connected to DB'))
 .catch(console.error())
 
